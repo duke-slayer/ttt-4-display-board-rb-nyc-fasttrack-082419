fa00b4d279774e6ecb4b2e44a3a9e1d3e97bd0c2
Amy Duke <amyyyduke@gmail.com>
2019-08-24 17:47:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
 def display_board
   puts "   |   |   "
   puts "-----------"
@@ -7,3 +8,4 @@ def display_board
   puts "-----------"
   puts "   |   |   "
 end
+display_board(board)
